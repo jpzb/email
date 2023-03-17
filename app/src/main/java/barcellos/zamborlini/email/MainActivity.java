3a86d89b3d74cde48183c44813ced3ce6c9e4680
20211tiimi0087 <jpzbarcellos@gmail.com>
2023-03-17 11:07:57 -0300

    Versão final

--- app/src/main/java/barcellos/zamborlini/email/MainActivity.java
+++ app/src/main/java/barcellos/zamborlini/email/MainActivity.java
@@ -18,23 +18,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btnEnviar = (Button) findViewById(R.id.btnEnviar);
+        Button btnEnviar = (Button) findViewById(R.id.btnEnviar); // Recebe o botão da tela.
 
         btnEnviar.setOnClickListener(new View.OnClickListener() {
+            // Coloca  o evento para pegar click no botão
+
             @Override
             public void onClick(View view) {
-                EditText etEmail = findViewById(R.id.etEmail);
-                String email = etEmail.getText().toString();
+                EditText etEmail = findViewById(R.id.etEmail); // Pega o editText de email
+                String email = etEmail.getText().toString(); // Pega o texto escrito no editText
 
-                EditText etAssunto = findViewById(R.id.etAssunto);
-                String assunto = etAssunto.getText().toString();
+                EditText etAssunto = findViewById(R.id.etAssunto); // Pega o editText de assunto
+                String assunto = etAssunto.getText().toString(); // Pega o texto escrito no editText
 
-                EditText etTexto = findViewById(R.id.etTexto);
-                String texto = etTexto.getText().toString();
+                EditText etTexto = findViewById(R.id.etTexto); // Pega o editText de texto
+                String texto = etTexto.getText().toString(); // Pega o texto escrito no editText
 
-                Intent i = new Intent(Intent.ACTION_SENDTO);
+                Intent i = new Intent(Intent.ACTION_SENDTO); // Cria a intenção
 
                 i.setData(Uri.parse("mailto:"));
+                // Colocar a ação a ser realizada para enviar email
 
                 String[] emails = new String[]{email};
                 i.putExtra(Intent.EXTRA_EMAIL, emails);
@@ -43,9 +46,11 @@ public class MainActivity extends AppCompatActivity {
 
                 try{
                     startActivity(Intent.createChooser(i, "Escolha o APP"));
+                    // Aparece todos os apps que podem enviar email para o usuário escolher
                 }catch (ActivityNotFoundException e){
                     Toast.makeText(MainActivity.this, "Não há nenhum app que posso " +
                             "realizar essa operação", Toast.LENGTH_LONG).show();
+                    // Não achou nenhum app de email
                 }
             }
         });
